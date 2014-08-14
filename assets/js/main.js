082cfcd1a1f0e250ecd0ace508627ce4effe241f
Jaden Dessureault <jaden.dessureault@gmail.com>
2014-08-13 19:59:11 -0500

    Add parallax conditional & fix mobile issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,11 @@ if (Math.round(Math.random())) {
   $("body").addClass("dark");
 }
 
-$(document).on("mousemove", function(e) {
-  var x = e.pageX * -1 / 100
-      y = e.pageY * -1 / 100;
+if ($(window).width() > 768) {
+  $(document).on("mousemove", function(e) {
+    var x = e.pageX * -1 / 100
+        y = e.pageY * -1 / 100;
 
-  $(".hero-image").css("-webkit-transform", "translate3d("+x+"px, "+y+"px, 0px)");
-});
+    $(".hero-image").css("-webkit-transform", "translate3d("+x+"px, "+y+"px, 0px)");
+  });
+}
