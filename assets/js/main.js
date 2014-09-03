90207d5113c447ed236072ae7e39155ca86a2268
Jaden Dessureault <jaden.dessureault@gmail.com>
2014-09-02 21:03:27 -0500

    Cool new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+// Randomize
 if (Math.round(Math.random())) {
-  document.body.className += ' dark';
+  $("body").addClass("light");
 }
+
+$("body").on("mousemove", function() {
+  var x = event.clientX,
+      y = event.clientY;
+
+  // Subtract the circle's radius
+  $('.blur').css({
+    "top": (y-75)+"px",
+    "left": (x-75)+"px"
+  });
+});
