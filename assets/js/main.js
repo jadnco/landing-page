8503fdb9a617d7ffda3d5e164d0169f63b294852
Jaden Dessureault <jaden.dessureault@gmail.com>
2014-09-05 16:13:35 -0500

    Remove cursor blur & jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,4 @@
 // Randomize
 if (Math.round(Math.random())) {
-  $("body").addClass("light");
+  document.body.className += " light";
 }
-
-$("body").on("mousemove", function() {
-  var x = event.clientX,
-      y = event.clientY;
-
-  // Subtract the circle's radius
-  $('.blur').css({
-    "top": (y-75)+"px",
-    "left": (x-75)+"px"
-  });
-});
